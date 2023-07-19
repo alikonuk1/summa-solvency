2ab71e47ba0fa0e21e8b7ba3a5f9d9ac3a0d2405
alikonuk1 <alikonuk1@gmail.com>
2023-07-19 17:30:52 +0300

    optimize the summa contract

--- contracts/src/Summa.sol
+++ contracts/src/Summa.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.18;
 // Uncomment this line to use console.log
 //import "hardhat/console.sol";
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol"; 
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
@@ -35,14 +35,16 @@ contract Summa is Ownable {
         bytes[] memory cexSignatures,
         string memory message
     ) public {
+        uint256 _lenCexAddresses = _cexAddresses.length;
+        uint256 lenCexAddresses = cexAddresses.length;
         require(
-            _cexAddresses.length == cexSignatures.length &&
-                _cexAddresses.length > 0,
+            _lenCexAddresses == cexSignatures.length &&
+                _lenCexAddresses > 0,
             "CEX addresses and signatures count mismatch"
         );
 
-        for (uint i = 0; i < _cexAddresses.length; i++) {
-            if (i >= cexAddresses.length) {
+        for (uint i = 0; i < _lenCexAddresses;) {
+            if (i >= lenCexAddresses) { 
                 cexAddresses.push(_cexAddresses[i]);
             } else if (_cexAddresses[i] != cexAddresses[i]) {
                 cexAddresses[i] = _cexAddresses[i];
@@ -54,13 +56,19 @@ contract Summa is Ownable {
                 _cexAddresses[i] == recoveredPubKey,
                 "Invalid signer for ETH address"
             );
+            unchecked {
+                ++i;
+            }
         }
 
         // Since we're always rewriting the old array with the new values, we need to make sure that we remove any leftovers if the new set of addresses is smaller than the old one
         // TODO - explore some gas-efficient ways of maintaining this array
-        if (_cexAddresses.length < cexAddresses.length) {
-            for (uint i = _cexAddresses.length; i < cexAddresses.length; i++) {
+        if (_lenCexAddresses < lenCexAddresses) {
+            for (uint i = _lenCexAddresses; i < lenCexAddresses;) {
                 cexAddresses.pop();
+                unchecked {
+                    ++i;
+                }
             }
         }
 
@@ -80,22 +88,30 @@ contract Summa is Ownable {
         uint256 mstRoot,
         bytes memory proof
     ) public {
+        uint256 lenAssetSums = assetSums.length;
+        uint256 lenErc20ContractAddresses = erc20ContractAddresses.length;
         require(
-            erc20ContractAddresses.length == assetSums.length - 1 &&
-                erc20ContractAddresses.length > 0,
+            lenErc20ContractAddresses == lenAssetSums - 1 &&
+                lenErc20ContractAddresses > 0,
             "ERC20 addresses and balances count mismatch"
         );
 
         uint256 totalETHBalance = 0;
         uint256[] memory erc20Balances = new uint256[](
-            erc20ContractAddresses.length
+            lenErc20ContractAddresses
         );
-        for (uint i = 0; i < cexAddresses.length; i++) {
+        for (uint i = 0; i < cexAddresses.length;) {
             totalETHBalance += cexAddresses[i].balance;
-            for (uint j = 0; j < erc20ContractAddresses.length; j++) {
+            for (uint j = 0; j < lenErc20ContractAddresses;) {
                 erc20Balances[j] += IERC20(erc20ContractAddresses[j]).balanceOf(
                     cexAddresses[i]
                 );
+                unchecked {
+                    ++j;
+                }
+            }
+            unchecked {
+                ++i;
             }
         }
 
@@ -104,18 +120,24 @@ contract Summa is Ownable {
             "Actual ETH balance is less than the proven balance"
         );
 
-        for (uint i = 0; i < erc20ContractAddresses.length; i++) {
+        for (uint i = 0; i < lenErc20ContractAddresses;) {
             require(
                 erc20Balances[i] >= assetSums[i + 1],
                 "Actual ERC20 balance is less than the proven balance"
             );
+            unchecked {
+                ++i;
+            }
         }
 
-        uint256[] memory inputs = new uint256[](assetSums.length + 1);
+        uint256[] memory inputs = new uint256[](lenAssetSums + 1);
         inputs[0] = mstRoot;
 
-        for (uint i = 0; i < assetSums.length; i++) {
+        for (uint i = 0; i < lenAssetSums;) {
             inputs[i + 1] = assetSums[i];
+            unchecked {
+                ++i;
+            }
         }
 
         // Verify ZK proof
